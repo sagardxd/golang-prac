a04f022d29bb2644ba26f82970f7ea8ff570e7d9
Sagar Yadav <sagardxd5@gmail.com>
2024-03-08 11:03:19 +0530

    09

--- 09Slices/main.go
+++ 09Slices/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("hi")
+
+	var fruits = []string{"Tomato", "Apple", "Peach"}
+	fruits = append(fruits, "mango")
+
+	fmt.Println(fruits)
+
+	fruits = append(fruits[1:4])
+	fmt.Println(fruits)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 300
+	highScores[1] = 900
+	highScores[2] = 100
+	highScores[3] = 200
+
+	fmt.Println(highScores)
+	highScores = append(highScores, 211, 1000, 345)
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+
 }
